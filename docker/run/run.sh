8befb170484708f3af2765b726c91f703133c4a7
Puteulanus <mj@puteulanus.com>
2017-02-01 16:04:40 +0000

    fix path

--- docker/run/run.sh
+++ docker/run/run.sh
@@ -6,11 +6,11 @@ sed -i "s/21232f297a57a5a743894a0e4a801fc3/$(cat /etc/passwd.txt | md5sum | awk 
 
 # Public password
 curl 'http://gb.weather.gov.hk/cgi-bin/hko/localtime.pl' > /usr/www/default/public_html/time.txt
-openssl rsautl -encrypt -in /etc/passwd.txt -inkey /var/run/RSA.pub -pubin -out /usr/www/default/public_html/password.txt
+openssl rsautl -encrypt -in /etc/passwd.txt -inkey /var/run/box-maker/RSA.pub -pubin -out /usr/www/default/public_html/password.txt
 
 # Clean
 rm -rf /etc/passwd.txt
-rm -rf /var/run/RSA.pub
+rm -rf /var/run/box-maker/RSA.pub
 
 # Run heart beat script
 bash /var/run/box-maker/heartbeat.sh >/dev/null 2>&1 &
