b5a96cb66bcf5d130b5f638dc1719b535389158c
Puteulanus <mj@puteulanus.com>
2017-02-05 14:14:44 +0000

    add ruTorrent install

--- docker/script/rtorrent-install.sh
+++ docker/script/rtorrent-install.sh
@@ -4,6 +4,12 @@
 yum install rtorrent -y
 
 # Configuration
-mkdir -p /usr/www/rtorrent/{.session,torrent,download}
+mkdir -p /usr/www/rtorrent/{.session,torrent,download,ruTorrent}
 cp /tmp/conf/rtorrent.rc /usr/www/.rtorrent.rc
+cd /usr/www/rtorrent/ruTorrent/
+wget -O ruTorrent.zip 'https://github.com/Novik/ruTorrent/archive/master.zip'
+unzip ruTorrent.zip
+mv ruTorrent-master/* ./
+mv ruTorrent-master/.htaccess ./
+rm -rf ruTorrent-master
 chown -R www:www /usr/www/
