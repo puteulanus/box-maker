3bced1a65f78e29ba2dcdb3d2ec72fc841472eea
Puteulanus <mj@puteulanus.com>
2017-02-05 15:14:26 +0000

    fix path link

--- docker/script/rtorrent-install.sh
+++ docker/script/rtorrent-install.sh
@@ -11,5 +11,12 @@ wget -O ruTorrent.zip 'https://github.com/Novik/ruTorrent/archive/master.zip'
 unzip ruTorrent.zip
 mv ruTorrent-master/* ./
 mv ruTorrent-master/.htaccess ./
-rm -rf ruTorrent-master
+rm -rf ruTorrent-master ruTorrent.zip
+
+ln -s /usr/www/rtorrent/ruTorrent /usr/www/default/public_html/ruT
+rm -rf /usr/www/default/public_html/data/User/admin/home/download
+ln -s /usr/www/rtorrent/download /usr/www/default/public_html/data/User/admin/home/download
+
+chown -h www:www /usr/www/default/public_html/ruT
+chown -h www:www /usr/www/default/public_html/data/User/admin/home/download
 chown -R www:www /usr/www/
