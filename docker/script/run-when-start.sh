7041f6a6846d48f4b966f02de5ac4afb6ed7662f
Puteulanus <mj@puteulanus.com>
2017-02-06 05:27:56 +0000

    test nginx auth

--- docker/script/run-when-start.sh
+++ docker/script/run-when-start.sh
@@ -12,4 +12,4 @@ echo 'files = supervisord.d/*.ini' >> /etc/supervisord.conf
 cp /tmp/conf/nginx-php.ini /etc/supervisord.d/nginx-php.ini
 cp /tmp/conf/transmission.ini /etc/supervisord.d/transmission.ini
 cp /tmp/conf/c9.ini /etc/supervisord.d/c9.ini
-#cp /tmp/conf/rtorrent.ini /etc/supervisord.d/rtorrent.ini
+cp /tmp/conf/rtorrent.ini /etc/supervisord.d/rtorrent.ini
