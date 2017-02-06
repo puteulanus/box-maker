325613191517652fb7262c3418909903b81f71fe
Puteulanus <mj@puteulanus.com>
2017-02-06 16:15:41 +0000

    add mkdir db filder

--- docker/run/run.sh
+++ docker/run/run.sh
@@ -7,11 +7,13 @@ sed -i "s/THE_PASSWORD/$(cat /etc/passwd.txt)/g" /etc/supervisord.d/c9.ini
 htpasswd -nb admin $(cat /etc/passwd.txt) >> /usr/www/default/public_html/ruT/.htpasswd
 chown www:www /usr/www/default/public_html/ruT/.htpasswd
 mv /var/run/box-maker/crypt.js /usr/www/rtorrent/flood/
+mkdir /usr/www/rtorrent/flood/server/db
 cat <<_EOF_ > /usr/www/rtorrent/flood/server/db/users.db
 {"username":"admin","password":"THE_PASSWORD","_id":"IBYplwpjv2PdOOAX"}
 {"$$indexCreated":{"fieldName":"username","unique":true,"sparse":false}}
 _EOF_
 sed -i "s/THE_PASSWORD/$(/usr/bin/node /usr/www/rtorrent/flood/crypt.js $(cat /etc/passwd.txt))/g" /usr/www/rtorrent/flood/server/db/users.db
+chown -R www:www /usr/www/rtorrent/flood/server/db
 rm -f /usr/www/rtorrent/flood/crypt.js
 
 # Public password
