b378dd294e4ea5a181bf821d54a7d4e8acc2a6e5
Puteulanus <mj@puteulanus.com>
2017-02-07 13:03:40 +0000

    nginx proxy flood

--- docker/conf/config.js
+++ docker/conf/config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-  baseURI: '/',
+  baseURI: '/shell//',
   dbCleanInterval: 1000 * 60 * 60,
   dbPath: './server/db/',
-  floodServerPort: 4443,
+  floodServerPort: 3000,
   maxHistoryStates: 30,
   pollInterval: 1000 * 5,
   secret: 'flood',
   scgi: {
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: 5000,
     socket: false,
     socketPath: '/tmp/rtorrent.sock'
