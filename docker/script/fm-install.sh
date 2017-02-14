51605028d171fb2e832016d93da3b82df2cff7cb
Puteulanus <mj@puteulanus.com>
2017-02-14 07:32:10 +0000

    remove system_setting.php

--- docker/script/fm-install.sh
+++ docker/script/fm-install.sh
@@ -13,15 +13,16 @@ rm -rf KODExplorer-master
 touch /usr/www/default/public_html/data/system/install.lock
 sed -i "s#define('KOD_SESSION',   DATA_PATH .'session/');#define('KOD_SESSION',   '/tmp/' .'session/');#" /usr/www/default/public_html/config/config.php
 echo -e "\n\n\$config['setting_system_default']['new_user_app'] = '';" >> /usr/www/default/public_html/config/setting.php
+echo "\$config['setting_default']['recycle_open'] = '0';" >> /usr/www/default/public_html/config/setting.php
 echo 'unset($config["setting_menu_default"][3]);' >> /usr/www/default/public_html/config/setting.php
 mkdir -p /usr/www/default/public_html/data/User/admin/{data,home,recycle}
 mkdir /usr/www/default/public_html/data/User/admin/home/desktop
 rm -f /usr/www/default/public_html/data/system/system_member.php
 rm -f /usr/www/default/public_html/data/system/system_group.php
-rm -f /usr/www/default/public_html/data/system/system_setting.php
+#rm -f /usr/www/default/public_html/data/system/system_setting.php
 cp /tmp/conf/fm/system_member.php /usr/www/default/public_html/data/system/system_member.php
 cp /tmp/conf/fm/system_group.php /usr/www/default/public_html/data/system/system_group.php
-cp /tmp/conf/fm/system_setting.php /usr/www/default/public_html/data/system/system_setting.php
+#cp /tmp/conf/fm/system_setting.php /usr/www/default/public_html/data/system/system_setting.php
 cp /tmp/conf/fm/config.php /usr/www/default/public_html/data/User/admin/data/config.php
 
 cp /tmp/conf/fm/*.oexe /usr/www/default/public_html/data/User/admin/home/desktop/
