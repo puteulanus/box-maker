27a535601e2a5cb2a9738ed54803258ca1fd9633
Puteulanus <mj@puteulanus.com>
2018-04-09 15:43:47 +0800

    fix nginx user

--- docker/script/nginx-install.sh
+++ docker/script/nginx-install.sh
@@ -5,7 +5,7 @@ yum install -y nginx
 
 # Main Configure
 mkdir -p /root/default/{public_html,logs}
-sed -i "s/#user nginx/user root root/g" /etc/nginx/nginx.conf
+sed -i "s/user nginx/user root root/g" /etc/nginx/nginx.conf
 
 # Site Configure
 sed -i 's/default_server//' /etc/nginx/nginx.conf
