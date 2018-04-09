568ebae34b74d14afea08136e8f90ced308e4c36
Puteulanus <mj@puteulanus.com>
2018-04-09 16:23:25 +0800

    fix nodejs install

--- docker/script/flood-install.sh
+++ docker/script/flood-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
 yum install nodejs -y
 mkdir /root/rtorrent/flood
 cd /root/rtorrent/flood
@@ -9,7 +9,9 @@ wget -O flood.zip 'https://github.com/jfurrow/flood/archive/master.zip'
 unzip flood.zip
 mv flood-master/* ./
 rm -rf flood-master flood.zip
+npm install -g node-gyp
 
 # Configure
 cp /tmp/conf/config.js /root/rtorrent/flood/
-npm install --production
+npm install
+npm run build
