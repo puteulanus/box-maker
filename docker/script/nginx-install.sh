6e0c4d0065b8717cbcdba866ff3791695406e253
Puteulanus <mj@puteulanus.com>
2018-04-09 15:20:11 +0800

    fix nginx user

--- docker/script/nginx-install.sh
+++ docker/script/nginx-install.sh
@@ -5,11 +5,7 @@ yum install -y nginx
 
 # Main Configure
 mkdir -p /root/default/{public_html,logs}
-sed -i "s/#user  nobody/user  root root/g" /etc/nginx/nginx.conf
-sed -i "s/#worker_processes  1/worker_processes  4/g" /etc/nginx/nginx.conf
-sed -i "s/#error_log  logs\/error.log/error_log  logs\/error.log crit/g" /etc/nginx/nginx.conf
-sed -i "s/#pid/pid/g" /etc/nginx/nginx.conf
-sed -i '/worker_connections/ i use epoll;' /etc/nginx/nginx.conf
+sed -i "s/#user nginx/user root root/g" /etc/nginx/nginx.conf
 
 # Site Configure
 sed -i 's/default_server//' /etc/nginx/nginx.conf
